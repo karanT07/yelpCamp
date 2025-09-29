78139ef70b150fcc7bed49985f4ddf40278e3f1f
Karan <realkaran07@gmail.com>
2025-09-30 01:02:55 +0530

    Image Resizing

--- models/campground.js
+++ models/campground.js
@@ -7,7 +7,7 @@ const ImageSchema = new Schema({
     filename: String
 });
 ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
+    return this.url.replace('/upload', '/upload/w_200,h_200');
 })
 
 const CampgroundSchema = new Schema({
